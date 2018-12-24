cdef8020ebf4b12db29a38800a38314f9cfe3d89
kali <alik9807274@gmail.com>
2018-12-23 23:45:40 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -1,43 +1,60 @@
 // Responsive Nav
-$(function() {
-  menu = $('nav ul');
+$(function () {
+    menu = $('nav ul');
 
-  $('#openup').on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
+    $('#openup').on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
 
-  $(window).resize(function() {
-    var w = $(this).width();
-    if (w > 480 && menu.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-  });
+    $(window).resize(function () {
+        var w = $(this).width();
+        if (w > 480 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
 
-  $('nav li').on('click', function(e) {
-    var w = $(window).width();
-    if (w < 480) {
-      menu.slideToggle();
-    }
-  });
-  $('.open-menu').height($(window).height());
+    $('nav li').on('click', function (e) {
+        var w = $(window).width();
+        if (w < 480) {
+            menu.slideToggle();
+        }
+    });
+    $('.open-menu').height($(window).height());
 });
 
 // Smooth Scrolling
-$('.cf a').on('click', function(event) {
-  if (this.hash !== '') {
-    event.preventDefault();
+$('.cf a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
 
-    const hash = this.hash;
+        const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      800,
-      function() {
-        window.location.hash = hash;
-      }
-    );
-  }
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function () {
+                window.location.hash = hash;
+            }
+        );
+    }
 });
+
+var sendEMail = function () {
+    window.open('mailto:' + "sfurs@sfu.ca" + '?subject=' + document.getElementById("subjectLine").value + '&body=' + document.getElementById("msg").value);
+};
+
+
+/*
+TODO
+SFU red color theme (some how)
+add timeline
+ */
+
+
+/*
+            <p class="text-light">lori ipsum.....</p>
+
+ */
